04795c5d40d9471f68fd07a0642cfbc29d580e9f
rzhang57 <rmzhang@student.ubc.ca>
2025-06-25 23:25:09 +0800

    add retries and fix login logic

--- services/ig_client.py
+++ services/ig_client.py
@@ -1,7 +1,7 @@
 from instagrapi import Client
 from dotenv import load_dotenv
 import os
-import requests
+import time
 
 load_dotenv()
 SESSION_FILE = "session.json"
@@ -25,25 +25,39 @@ def get_ig_client() -> Client:
             return cl
         except Exception as e:
             print(f"⚠️ Failed to reuse session: {e}, retrying fresh login")
-
-    cl.login(username, password)
-    cl.dump_settings(SESSION_FILE)
-    print("🔐 Logged in and saved new session")
-    return cl
-
+            os.remove(SESSION_FILE)
+    try:
+        cl = Client()
+        cl.login(username, password)
+        cl.dump_settings(SESSION_FILE)
+        print("🔐 Logged in and saved new session")
+        return cl
+    except Exception as login_err:
+        print(f"❌ Login failed: {login_err}")
+        raise
 
 def download_video_media(cl: Client, url: str) -> str:
-    print("starting download process")
-    ensure_download_dir()
+    attempt = 0
 
-    media_pk = cl.media_pk_from_url(url)
-    media_info = cl.media_info(media_pk)
+    while attempt < 4:
+        print(f"starting download process attempt {attempt}")
+        try:
+            ensure_download_dir()
+            media_pk = cl.media_pk_from_url(url)
+            media_info = cl.media_info(media_pk)
+            video_url = media_info.video_url
+            path = cl.clip_download_by_url(video_url, folder=DOWNLOAD_DIR)
+            return path
+        except Exception as e:
+            print(f"attempt {attempt} failed to download due to {e}")
+            attempt += 1
+            time.sleep(2)
+    
+    raise Exception("failed to download")
 
-    video_url = media_info.video_url
-    path = cl.clip_download_by_url(video_url, folder=DOWNLOAD_DIR)
-    return path
+            
 
-def upload_video(cl: Client, video_path: str, caption: str = "Recognize me? Follow for the best curated memes! @rhgpu"):
+def upload_video(cl: Client, video_path: str, caption: str = "follow before i disappear forever (@suswindow)"):
     print("starting upload process")
 
     if not os.path.exists(video_path):
@@ -53,9 +67,11 @@ def upload_video(cl: Client, video_path: str, caption: str = "Recognize me? Foll
         print ("uploading...")
         cl.clip_upload(video_path, caption=caption)
         print(f"✅ Uploaded: {video_path}")
+        os.remove(video_path)
         return {"status": "uploaded", "path": video_path}
     except Exception as e:
         print(f"❌ Upload failed: {e}")
+        os.remove(video_path)
         raise Exception("Upload failed")
 
 def ensure_download_dir():
